43bb5a9a71309b47fccedd830fafb76fd391da49
yaphet17 <yafetberhanu3@gmail.com>
2022-10-05 13:06:41 +0300

    initial commit

---
+++ src/main/java/com/yaphet/strategymatrixpoc/StrategyMatrixPocApplication.java
@@ -0,0 +1,13 @@
+package com.yaphet.strategymatrixpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyMatrixPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrategyMatrixPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yaphet/strategymatrixpoc/StrategyMatrixPocApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yaphet.strategymatrixpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyMatrixPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("==========================Hello Java==================================");
+    }
+
+}
